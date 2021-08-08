6d56fa535462a0bdbc645f4a22dbdc4f9e3b82b6
Louis Bailleau <louis.bailleau@free.fr>
2021-08-08 18:52:51 +0200

    Improved navbar
    - Imported bootstrap colors for default theme
    - Added mobile support for navbar

--- resources/js/store/components/NavBar/index.tsx
+++ resources/js/store/components/NavBar/index.tsx
@@ -5,35 +5,71 @@ import { CombinedState } from 'redux'
 import { RootState } from '@/store/redux'
 import { connect } from 'react-redux'
 import { withTranslation, I18nextProviderProps } from 'react-i18next'
+import classNames from 'classnames'
 
 const mapStateToProps = (state: RootState): CombinedState<GlobalState> => state.global
 type NavbarProps = ReturnType<typeof mapStateToProps> & I18nextProviderProps
 
-class Navbar extends React.Component<NavbarProps> {
+interface NavbarState {
+  mobileOpen: boolean
+  submenu: string
+}
+
+class Navbar extends React.Component<NavbarProps, NavbarState> {
+  public state: NavbarState = {
+    mobileOpen: false,
+    submenu: '',
+  }
+
+  private openSub(sub: string): void {
+    if (this.state.submenu === sub) {
+      this.setState({ submenu: '' })
+    } else {
+      this.setState({ submenu: sub })
+    }
+  }
+
+  private isSub(sub: string): boolean {
+    return this.state.submenu === sub
+  }
+
   public render(): JSX.Element {
     const i18n = this.props.i18n
-    console.log(i18n)
-    i18n.t('')
+
     return (
       <nav className="navbar">
         <div className="container">
           <div className="navbar-brand">
-            <NavLink to="/" className="navbar-item">
+            <NavLink to="/" className="navbar-item logo">
               <img src={this.props.appIcon} alt={`${this.props.appName}'s Logo`} />
               <span className="brand-text">{this.props.appName}</span>
             </NavLink>
 
-            <button role="button" className="navbar-burger" data-target="nav-menu">
+            <button
+              role="button"
+              className="navbar-burger"
+              data-target="nav-menu"
+              onClick={() => this.setState({ mobileOpen: !this.state.mobileOpen })}
+            >
               <i className="fas fa-bars"></i>
             </button>
           </div>
 
-          <div id="nav-menu" className="navbar-menu">
+          <div
+            id="nav-menu"
+            className={classNames('navbar-menu', { 'is-active': this.state.mobileOpen })}
+          >
             <div className="navbar-start">
               <div className="navbar-item has-dropdown is-hoverable">
-                <button className="navbar-link">{i18n.t('store:components.navbar.plans')}</button>
+                <button className="navbar-link" onClick={() => this.openSub('plans')}>
+                  {i18n.t('store:components.navbar.plans')}
+                </button>
 
-                <div className="navbar-dropdown">
+                <div
+                  className={classNames('navbar-dropdown', {
+                    'is-active': this.isSub('plans'),
+                  })}
+                >
                   <NavLink to="/plans" className="navbar-item">
                     {i18n.t('store:components.navbar.all_plans')}
                   </NavLink>
@@ -56,16 +92,26 @@ class Navbar extends React.Component<NavbarProps> {
 
             <div className="navbar-end">
               <div className="navbar-item has-dropdown is-hoverable">
-                <button className="navbar-link">
+                <button className="navbar-link" onClick={() => this.openSub('currency')}>
                   {i18n.t('store:components.navbar.currency')}
                 </button>
-                <div className="has-dropdown"></div>
+                <div
+                  className={classNames('navbar-dropdown', {
+                    'is-active': this.isSub('currency'),
+                  })}
+                ></div>
               </div>
 
               <div className="navbar-item has-dropdown is-hoverable">
-                <button className="navbar-link">{i18n.t(`langs.${i18n.language}`)}</button>
+                <button className="navbar-link" onClick={() => this.openSub('languages')}>
+                  {i18n.t(`langs.${i18n.language}`)}
+                </button>
 
-                <div className="navbar-dropdown">
+                <div
+                  className={classNames('navbar-dropdown', {
+                    'is-active': this.isSub('languages'),
+                  })}
+                >
                   {this.props.i18n.languages.map((language, index) => (
                     <button className="navbar-item" key={index}>
                       {i18n.t(`langs.${language}`)}
@@ -75,8 +121,14 @@ class Navbar extends React.Component<NavbarProps> {
               </div>
 
               <div className="navbar-item has-dropdown is-hoverable">
-                <button className="navbar-link">{i18n.t('store:components.navbar.account')}</button>
-                <div className="navbar-dropdown">
+                <button className="navbar-link" onClick={() => this.openSub('account')}>
+                  {i18n.t('store:components.navbar.account')}
+                </button>
+                <div
+                  className={classNames('navbar-dropdown', {
+                    'is-active': this.isSub('account'),
+                  })}
+                >
                   <NavLink to="/login" className="navbar-item">
                     <i className="fas fa-signin"></i>
                     {i18n.t('store:components.navbar.login')}
